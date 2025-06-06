2abaa3f0b844adc19fd6ac9fe4c9f6b069648dcc
zhangxueliang <3011549907@qq.com>
2025-06-07 01:05:31 +0800

    新增连接数限制

--- main.go
+++ main.go
@@ -4,17 +4,19 @@ import (
 	"errors"
 	"net"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type TCPPoolConn struct {
-	mu       sync.Mutex
-	addr     string
-	pool     chan net.Conn
-	maxConns int
+	mu        sync.Mutex
+	addr      string
+	pool      chan net.Conn
+	maxConns  int64
+	currConns int64 // 当前总连接数
 }
 
-func NewTCPConnPool(addr string, maxConns int) *TCPPoolConn {
+func NewTCPConnPool(addr string, maxConns int64) *TCPPoolConn {
 	return &TCPPoolConn{
 		addr:     addr,
 		pool:     make(chan net.Conn, maxConns),
@@ -44,12 +46,22 @@ func (p *TCPPoolConn) Put(conn net.Conn) error {
 		return nil
 	default:
 		conn.Close()
+		atomic.AddInt64(&p.currConns, -1)
 		return errors.New("pool full")
 	}
 }
 
 func (p *TCPPoolConn) newConn() (net.Conn, error) {
-	return net.DialTimeout("tcp", p.addr, 2*time.Second)
+	if atomic.LoadInt64(&p.currConns) > p.maxConns {
+		return nil, errors.New("当前连接数超过最大允许连接数")
+	}
+
+	conn, err := net.DialTimeout("tcp", p.addr, 2*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	atomic.AddInt64(&p.currConns, 1)
+	return conn, nil
 }
 
 func isDead(conn net.Conn) bool {
